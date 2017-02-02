8aa0712213ab3844376c7c69357be78cb4bfe8fb
loveAshley <loveashleyu@gmail.com>
2017-02-02 02:59:24 +0000

    second commit

--- assign3.js
+++ assign3.js
@@ -1,24 +1,25 @@
 "use strict";
 
-// function myLoop (array)
-// {
-//     for ( var i = 0; i < array.length; i++) {
-//          array[i] = array[i]  + 2;
-      
-//     }
-//     console.log (array); 
-// }
-
-// let arr = [ 1, 2, 3 ]
-// myLoop (arr)
-function arrSum(a)
+function myLoop (array)
 {
-    const myVal=process.argv
-a=[]
-
-a.push(Number(myVal[2])+2,Number(myVal[3])+2,Number(myVal[4])+2)
-
-console.log(a)
+    for ( var i = 0; i < array.length; i++) {
+         array[i] = array[i]  + 2;
+      
+    }
+    console.log (array); 
 }
-arrSum([])
+
+let arr = [ 1, 2, 3 ]
+myLoop (arr)
+
+// function arrSum(a)
+// {
+//     const myVal=process.argv
+// a=[]
+
+// a.push(Number(myVal[2])+2,Number(myVal[3])+2,Number(myVal[4])+2)
+
+// console.log(a)
+// }
+// arrSum([])
 
